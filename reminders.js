95da9538d1e18d74f313f5a9279267e867688094
Estelle Teglia <estelle.teglia@gmail.com>
2023-11-21 18:41:09 +0100

    trying some things

--- reminders.js
+++ reminders.js
@@ -6,3 +6,13 @@ const popupTry = () => {
 popupTry()
 
 
+const makeNotificationDisappear = () => {
+    //document.getElementsByClassName("buttonClose")
+    // this.parentElement.style.display = 'none';
+    // let notification = document.getElementsByClassName("buttonClose");
+    // for (let i = 0; i < notification.length; i++) {
+    //     notification[i].style.display = 'none';
+    // }
+}
+//document.getElementsByClassName("buttonClose").addEventListener("click", makeNotificationDisappear)
+
